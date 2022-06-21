ecbc5d172c089ed4f254079b2227c6ef3c9a38c8
SRmeraki <96946474+SRmeraki@users.noreply.github.com>
2022-06-21 21:06:05 +0530

    Add files via upload

---
+++ SR-initials-using-turtle/SRinitials.py
@@ -0,0 +1,13 @@
+
+import turtle as s
+# form S
+s.pensize(20)
+s.color('pink')
+s.circle(80,180)
+s.circle(180,80)
+s.circle(80,120)
+s.circle(-120,290)
+# form R
+s.forward(50)
+s.circle(-50,300)
+s.backward(100)
